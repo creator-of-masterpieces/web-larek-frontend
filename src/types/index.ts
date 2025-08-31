b8dbc3aec5215d30f3e1026cb0815eebcf952b0e
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-09-01 00:54:44 +0300

    Начал работу над документацией;

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
-// Интерфесы данных
+// Типы и интерфейсы данных
+
+// Способы оплаты
+export type TUserPayment = 'card' | 'cash' | '';
+
+// Карточка
 export interface ICard {
 	id: string;
 	title: string;
@@ -8,6 +13,7 @@ export interface ICard {
 	description: string;
 }
 
+// Интерфейс класса данных каталога карточек
 export interface ICardsData {
 	cards: ICard[];
 	preview: ICard;
@@ -17,9 +23,32 @@ export interface ICardsData {
 	getCard(id: string): ICard[];
 }
 
-export interface IUserData {
-	payment: 'card' | 'cash' | '';
+// Данные покупателя
+export interface IUser {
+	payment: TUserPayment
 	address: string;
 	email: string;
 	phone: string;
 }
+
+// Интерфейс класса данных покупателя
+export interface IUserData {
+	payment: TUserPayment
+	address: string;
+	email: string;
+	phone: string;
+	GetUser (): IUser;
+	ValidateUser (userData: IUser): boolean;
+	SaveUser (userData: IUser): void;
+}
+
+// Интерфейс класса данных корзины
+export interface IBasketData {
+	Cards: ICard[];
+	AddCard (card: ICard[]): void;
+	RemoveCard (id: string): void;
+	GetTotalPrice(cards: ICard[]): number;
+	GetCards (): ICard[];
+	IsInBasket (id: string): boolean;
+}
+
